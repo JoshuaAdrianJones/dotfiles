5c6569640cc0cfa98325b125384a12088d05188e
joshua <joshua.ar.jones@gmail.com>
2020-07-01 16:38:37 +0100

    removed shell script because it was bad

--- updateDotFiles.sh
+++
@@ -1,6 +0,0 @@
-cd ~
-cp .zshrc /Users/josh/Temp/dotfiles
-cp .vimrc /Users/josh/Temp/dotfiles
-cp .emacs /Users/josh/Temp/dotfiles
-cp -r .vim /Users/josh/Temp/dotfiles
-cp -r .emacs.d /Users/josh/Temp/dotfiles
